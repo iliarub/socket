1a1c9687fa6f173faac34ed4c6808061503924d4
iliarub <iliarub96@gmail.com>
2019-07-22 11:19:43 +0700

    create test

--- test_udp/server.c
+++ test_udp/server.c
@@ -13,13 +13,13 @@ int main()
 	serv.sin_addr.s_addr=inet_addr("127.0.0.1");
 	char buff[255]="messge from server";
 	int fd, fd_client;
+	char tmp[255];
 	int size = sizeof(client);
-	fd= socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	fd= socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	bind(fd, (struct sockaddr*)&serv, sizeof(serv));
-	listen(fd, 5);
-	fd_client=accept(fd, (struct sockaddr*)&client, &size);
-	send(fd_client, buff, 255, 0);
-	recv(fd_client, buff, 255, 0);
+	recvfrom(fd, tmp, 255, 0,(struct sockaddr*)&client , &size);
+	sendto(fd, buff, 255, 0,(struct sockaddr*) &client, sizeof(client));
+	recvfrom(fd, buff, 255, 0,(struct sockaddr*)&client , &size);
 	printf("%s\n", buff);
 	close(fd_client);
 	close(fd);
